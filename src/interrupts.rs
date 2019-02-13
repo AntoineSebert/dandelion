e3463add32244946a003009dcedbb06f44dafbe0
Antoine <antoine.sb@orange.fr>
2019-02-13 22:39:47 +0000

    :recycle: refactor `use` directives :sparkles: add real-time interrupts

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -6,43 +6,67 @@
 // configuration
 #![cfg(not(windows))]
 
-// crates
-// legacy
+// crate
 extern crate lazy_static;
 extern crate pc_keyboard;
 extern crate pic8259_simple;
 extern crate spin;
-// custom
-extern crate heapless;
 
-// uses
-// legacy
-use self::{lazy_static::lazy_static, pic8259_simple::ChainedPics};
-use crate::{gdt, hlt_loop, print, println};
+// use
+use crate::{hlt_loop, print, println};
+use lazy_static::lazy_static;
+use pic8259_simple::ChainedPics;
 use x86_64::structures::idt::{ExceptionStackFrame, InterruptDescriptorTable, PageFaultErrorCode};
 
 pub const PIC_1_OFFSET: u8 = 32; // 32 to 39
 pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8; // 40 to 47
 
-#[derive(Debug)]
-#[repr(u8)]
-pub enum HardwareInterrupt {
-	Timer = PIC_1_OFFSET,
-	Keyboard,
-	Other,
-	SerialPort2,
-	SerialPort1,
-	ParallelPort2_3,
-	FloppyDisk,
-	ParallelPort1,
-	RealTimeClock,
-	Acpi,
-	Available1,
-	Available2,
-	Mouse,
-	CoProcessor,
-	PrimaryAta,
-	SecondaryAta,
+pub mod interrupt_indexes {
+	#[derive(Debug, Clone, Copy)]
+	#[repr(u8)]
+	pub enum Hardware {
+		Timer = super::PIC_1_OFFSET,
+		Keyboard,
+		Other,
+		SerialPort2,
+		SerialPort1,
+		ParallelPort2_3,
+		FloppyDisk,
+		ParallelPort1,
+		RealTimeClock,
+		Acpi,
+		Available1,
+		Available2,
+		Mouse,
+		CoProcessor,
+		PrimaryAta,
+		SecondaryAta,
+	}
+	#[derive(Debug, Clone, Copy)]
+	#[repr(u8)]
+	pub enum RealTime {
+		HardDeadline = super::PIC_2_OFFSET + 8,
+		FirmDeadline,
+		SoftDeadline,
+		TimeRemaining, // with integer as power of 2 as code
+		WorkRemaining, // with integer as power of 2 as code
+		Periodic,      // with integer time interval as code
+		Aperiodic,
+	}
+	impl Hardware {
+		#[inline]
+		pub fn as_u8(self) -> u8 { self as u8 }
+
+		#[inline]
+		pub fn as_usize(self) -> usize { usize::from(self.as_u8()) }
+	}
+	impl RealTime {
+		#[inline]
+		pub fn as_u8(self) -> u8 { self as u8 }
+
+		#[inline]
+		pub fn as_usize(self) -> usize { usize::from(self.as_u8()) }
+	}
 }
 
 pub static PICS: spin::Mutex<ChainedPics> = spin::Mutex::new(unsafe { ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET) });
@@ -53,6 +77,7 @@ lazy_static! {
 	static ref IDT: InterruptDescriptorTable = {
 		let mut idt = InterruptDescriptorTable::new();
 		/* software */ {
+			use crate::gdt;
 			/*
 			idt.divide_by_zero.set_handler_fn();
 			idt.debug.set_handler_fn();
@@ -88,8 +113,9 @@ lazy_static! {
 			*/
 		}
 		/* hardware */ {
-			idt[usize::from(HardwareInterrupt::Timer as u8)].set_handler_fn(timer_interrupt_handler);
-			idt[usize::from(HardwareInterrupt::Keyboard as u8)].set_handler_fn(keyboard_interrupt_handler);
+			use interrupt_indexes::Hardware;
+			idt[Hardware::Timer.as_usize()].set_handler_fn(timer_interrupt_handler);
+			idt[Hardware::Keyboard.as_usize()].set_handler_fn(keyboard_interrupt_handler);
 		}
 
 		idt
@@ -111,15 +137,12 @@ extern "x86-interrupt" fn double_fault_handler(stack_frame: &mut ExceptionStackF
 
 extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: &mut ExceptionStackFrame) {
 	print!(".");
-	unsafe { PICS.lock().notify_end_of_interrupt(HardwareInterrupt::Timer as u8) }
+	unsafe { PICS.lock().notify_end_of_interrupt(interrupt_indexes::Hardware::Timer.as_u8()) }
 }
 
 extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: &mut ExceptionStackFrame) {
-	use self::{
-		pc_keyboard::{layouts, DecodedKey, Keyboard, ScancodeSet1},
-		spin::Mutex,
-	};
-	use x86_64::instructions::port::Port;
+	use pc_keyboard::{layouts, DecodedKey, Keyboard, ScancodeSet1};
+	use spin::Mutex;
 
 	lazy_static! {
 		static ref KEYBOARD: Mutex<Keyboard<layouts::Us104Key, ScancodeSet1>> =
@@ -127,7 +150,7 @@ extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: &mut Exceptio
 	}
 
 	let mut keyboard = KEYBOARD.lock();
-	let port = Port::new(0x60);
+	let port = x86_64::instructions::port::Port::new(0x60);
 
 	let scancode: u8 = unsafe { port.read() };
 	if let Ok(Some(key_event)) = keyboard.add_byte(scancode) {
@@ -138,15 +161,12 @@ extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: &mut Exceptio
 			}
 		}
 	}
-	unsafe { PICS.lock().notify_end_of_interrupt(HardwareInterrupt::Keyboard as u8) }
+	unsafe { PICS.lock().notify_end_of_interrupt(interrupt_indexes::Hardware::Keyboard.as_u8()) }
 }
 
 extern "x86-interrupt" fn page_fault_handler(stack_frame: &mut ExceptionStackFrame, _error_code: PageFaultErrorCode) {
-	use crate::hlt_loop;
-	use x86_64::registers::control::Cr2;
-
 	println!("EXCEPTION: PAGE FAULT");
-	println!("Accessed Address: {:?}", Cr2::read());
+	println!("Accessed Address: {:?}", x86_64::registers::control::Cr2::read());
 	println!("{:#?}", stack_frame);
 	hlt_loop();
 }
