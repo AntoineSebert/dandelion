628cb1c8c19b6afde184083efb43a0ca010f8337
Antoine <antoine.sb@orange.fr>
2019-02-17 21:17:01 +0000

    :white_check_mark: add test for realtime interrupts

---
+++ src/bin/test-task-remaining-exception.rs
@@ -0,0 +1,41 @@
+/*
+ * @author	Antoine "Anthony" Louis Thibaut Sébert
+ * @date	17/02/2019
+ */
+
+#![cfg_attr(not(test), no_std)]
+#![cfg_attr(not(test), no_main)]
+#![cfg_attr(test, allow(unused_imports))]
+
+extern crate dandelion;
+extern crate x86_64;
+
+use core::panic::PanicInfo;
+use dandelion::{exit_qemu, serial_println};
+
+#[cfg(not(test))]
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+	use dandelion::interrupts::{interrupt_indexes::RealTime, int};
+
+	int(RealTime::TaskRemaining);
+	serial_println!("ok");
+
+	unsafe {
+		exit_qemu();
+	}
+	loop {}
+}
+
+/// This function is called on panic.
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+	serial_println!("failed");
+	serial_println!("{}", _info);
+
+	unsafe {
+		exit_qemu();
+	}
+	loop {}
+}
