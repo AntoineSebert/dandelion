278b5faffda05953da7112b066eb5daaf2d579c2
Antoine <antoine.sb@orange.fr>
2019-03-09 15:50:21 +0000

    :construction: prepare process_table implementation

--- src/kernel/mod.rs
+++ src/kernel/mod.rs
@@ -9,4 +9,21 @@ pub mod shell;
 pub mod vmm;
 
 pub mod process;
-pub mod time;
+
+use lazy_static::lazy_static;
+
+// https://japaric.github.io/heapless/heapless/struct.IndexSet.html
+
+/*
+use rustc_hash::FxHasher;
+impl Hash for Process {
+	fn hash<H: FxHasher>(&self, state: &mut H) {
+		self.process_id.hash(state);
+	}
+}
+
+use rustc_hash::FxHashSet;
+lazy_static! {
+	pub static ref PROCESS_TABLE: FxHashSet<Process> = FxHashSet::default();
+}
+*/
