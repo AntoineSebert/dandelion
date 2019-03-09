4b204e27d61a57b54a049e05a0b4c9b3cbd22830
Antoine <antoine.sb@orange.fr>
2019-03-09 15:53:36 +0000

    :sparkles: basic time implementation based on `cmos` crate

--- src/kernel/time.rs
+++ src/kernel/time.rs
@@ -1,20 +1,13 @@
 /*
- * @author	Redox OS
- * @link	https://gitlab.redox-os.org
+ * @author	Antoine "Anthony" Louis Thibaut Sébert
+ * @date	06/03/2019
  */
 
-use spin::Mutex;
+// https://github.com/noahrinehart/cmos
+// https://docs.rs/cmos/0.1.1/cmos/
 
-/// Kernel start time, measured in (seconds, nanoseconds) since Unix epoch
-pub static START: Mutex<(u64, u64)> = Mutex::new((0, 0));
-/// Kernel up time, measured in (seconds, nanoseconds) since `START_TIME`
-pub static OFFSET: Mutex<(u64, u64)> = Mutex::new((0, 0));
+use cmos::{CMOS, CMOSCenturyHandler};
 
-pub fn monotonic() -> (u64, u64) { *OFFSET.lock() }
-
-pub fn realtime() -> (u64, u64) {
-	let offset = monotonic();
-	let start = *START.lock();
-	let sum = start.1 + offset.1;
-	(start.0 + offset.0 + sum / 1_000_000_000, sum % 1_000_000_000)
-}
+let mut cmos = unsafe { CMOS::new() };
+let rtc = cmos.read_rtc(CMOSCenturyHandler::CurrentYear(2019));
+println!("{:?}", rtc);
