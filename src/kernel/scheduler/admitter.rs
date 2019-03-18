c8b0adef82eed0be7d643a12859770ec5a88da3c
Antoine <antoine.sb@orange.fr>
2019-03-18 21:39:38 +0000

    :construction: `request()` and `get_duration()`

--- src/kernel/scheduler/admitter.rs
+++ src/kernel/scheduler/admitter.rs
@@ -3,16 +3,32 @@
  * @date	03/03/2019
  */
 
-/*
-pub fn request(job) {
+use crate::kernel::PROCESS_TABLE;
 
+pub type Arguments = [[char; 256]; 256];
+pub trait MainFn = Fn(Arguments) -> i64;
+
+/// Check whether the task can be accepted or not
+/// If yes, a process is constructed and add to the process queue & job table, and true is returned
+/// Otherwise, returns false.
+pub fn request<F>(function: F, args: Arguments) -> bool where F: MainFn {
+	let slot = get_slot();
+	if slot.is_none() {
+		return false;
+	}
+	// if not schedulable return false
+
+	// create a process and add it to process table & process queue
+
+	return true;
 }
-
-pub fn estimate_completion_time(job) {
-
+/// Browse PROCESS_TABLE. When an empty slot is found, its index is returned in an Option.
+/// If no empty slot exists, an empty Option is returned
+fn get_slot() -> Option<u8> {
+	for index in 0..256 {
+		if PROCESS_TABLE[index].lock().is_none() {
+			return Some(index as u8);
+		}
+	}
+	None
 }
-
-pub fn estimate_completion_time() {
-
-}
-*/
--- src/kernel/time.rs
+++ src/kernel/time.rs
@@ -4,10 +4,15 @@
  */
 
 use cmos::RTCDateTime;
+use core::time::Duration;
 
 pub fn get_datetime() -> RTCDateTime {
 	use crate::kernel::CMOS;
 	use cmos::CMOSCenturyHandler;
 
 	CMOS.lock().read_rtc(CMOSCenturyHandler::CurrentYear(2019))
+}
+
+pub fn get_duration(first: RTCDateTime, second:RTCDateTime) -> Duration {
+	Duration::new(0, 0)
 }
