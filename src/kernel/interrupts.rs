ee4cdd1ce5fd31129cf7adb89f1cfa56dd540d6a
Antoine <antoine.sb@orange.fr>
2019-09-14 14:14:13 +0200

    :sparkles: print error code on page fault

--- src/kernel/interrupts.rs
+++ src/kernel/interrupts.rs
@@ -129,11 +129,12 @@ extern "x86-interrupt" fn double_fault_handler(stack_frame: &mut InterruptStackF
 	hlt_loop();
 }
 
-extern "x86-interrupt" fn page_fault_handler(stack_frame: &mut InterruptStackFrame, _error_code: PageFaultErrorCode) {
+extern "x86-interrupt" fn page_fault_handler(stack_frame: &mut InterruptStackFrame, error_code: PageFaultErrorCode) {
 	use x86_64::registers::control::Cr2;
 
 	println!("EXCEPTION: PAGE FAULT");
 	println!("Accessed Address: {:?}", Cr2::read());
+	println!("Error Code: {:?}", error_code);
 	println!("{:#?}", stack_frame);
 
 	hlt_loop();
