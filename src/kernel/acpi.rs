cb19d77a53e50a924ad11122b80bca38fe3464f8
Antoine <antoine.sb@orange.fr>
2020-04-13 14:33:10 +0200

    :bulb: write safety doc

--- src/kernel/acpi.rs
+++ src/kernel/acpi.rs
@@ -2,4 +2,7 @@
 
 /// Initialise the ACPI.
 /// Returns an error message if the ACPI is not present or could not be initialised.
+///
+/// # Safety
+/// Dealing with hardware components.
 pub unsafe fn init() -> Result<(), &'static str> { Ok(()) }
