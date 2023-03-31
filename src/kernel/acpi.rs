189182e35e313b5ef1c5471f4c0eb39bd36c4eb4
Antoine <antoine.sb@orange.fr>
2023-03-31 18:26:35 +0200

    remove files

--- src/kernel/acpi.rs
+++
@@ -1,8 +0,0 @@
-// https://wiki.osdev.org/ACPI
-
-/// Initialise the ACPI.
-/// Returns an error message if the ACPI is not present or could not be initialised.
-///
-/// # Safety
-/// Dealing with hardware components.
-pub unsafe fn init() -> Result<(), &'static str> { Ok(()) }
